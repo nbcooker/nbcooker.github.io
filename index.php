67050655a6459751f520948380cac1066e6e2dc0
ray <nbcooker@gmail.com>
2014-10-21 21:55:09 +0800

    index.php

---
+++ index.php
@@ -0,0 +1,192 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Ibeautyou蓓欧缇手工蕾丝包</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="robots" content="all" />
+    <meta name="author" content="Ieautyou蓓欧缇" />
+    <meta name="description" content="Ibeautyou是蓓欧缇推出的手工蕾丝包包品牌，她由蓓欧缇新锐独立设计师萌大拿创立，为您定制最美丽的蕾丝包包。">
+    <meta name="Keywords" content="手工包包，蕾丝包包，定制包包，口金包包">
+    <link rel="apple-touch-icon-precomposed" href="icon152.png">
+    <link rel="shortcut icon" href="icon32.png">
+
+    <!-- Bootstrap core CSS -->
+    <!--<link href="../../dist/css/bootstrap.min.css" rel="stylesheet">-->
+    <link rel="stylesheet" href="http://cdn.bootcss.com/bootstrap/3.2.0/css/bootstrap.min.css">
+    <link href="carousel.css" rel="stylesheet">
+    <link href="unslide.css" rel="stylesheet">
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+    <!-- Custom styles for this template -->
+  </head>
+<!-- NAVBAR
+================================================== -->
+<?php 
+$shenai_taobao_url = 'http://item.taobao.com/item.htm?spm=a1z10.1.w5003-7907393001.1.Ivm1U9&id=37928692078&_u=3nahd39e2de&scene=taobao_shop';
+$moli_taobao_url = 'http://item.taobao.com/item.htm?spm=a1z10.1.w5003-7907393001.4.Ivm1U9&id=39617937260&scene=taobao_shop';
+$jian_taobao_url = 'http://item.taobao.com/item.htm?spm=a1z10.5.w4002-1386643438.29.4Tje0s&id=40032682732';
+>
+  <body>
+    <div class="navbar-wrapper">
+      <div class="container">
+
+        <div class="navbar navbar-inverse navbar-static-top" role="navigation" style="filter:alpha(opacity=80); -moz-opacity:0.8; opacity:0.8;">
+          <div class="container">
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+              <a class="navbar-brand hidden-sm" href="#">Ibeautyou</a>
+            </div>
+            <div class="navbar-collapse collapse">
+              <ul class="nav navbar-nav">
+                <li class="active"><a href="#">蓓欧缇</a></li>
+                <!--<li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown">产品<span class="caret"></span></a>
+                  <ul class="dropdown-menu" role="menu">
+                    <li><a href="deeplove.html">深爱</a></li>
+                    <li><a href="jasmine.html">茉莉</a></li>
+                  </ul>
+                </li>
+                <li><a href="#about">About</a></li>-->
+              </ul>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+
+
+<div class="banner">
+			<ul>
+				<li style="background-image: url('image_1.jpg');">
+				</li>
+
+				<li style="background-image: url('image_2.jpg');">
+				</li>
+			</ul>
+		</div>
+
+
+
+
+
+    <!-- Marketing messaging and featurettes
+    ================================================== -->
+    <!-- Wrap the rest of the page in another container to center all the content. -->
+    <div class="container marketing">
+<br />
+<br />
+
+      <!-- Three columns of text below the carousel -->
+      <div class="row">
+        <div class="col-lg-4">
+          <img class="img-circle" src="detail1.jpg" alt="Generic placeholder image" style="width: 200px; height: 200px;">
+          <h2>深    爱</h2>
+          <p>这是一款黑色，完全亚麻质地的蕾丝包包，她的名字叫做深爱，她，就会是你的深爱。</p>
+          <p><a class="btn btn-default" href="<?php echo $shenai_taobao_url; ?>" role="button">View details &raquo;</a></p>
+        </div><!-- /.col-lg-4 -->
+        <div class="col-lg-4">
+          <img class="img-circle" src="detail2.jpg" alt="Generic placeholder image" style="width: 200px; height: 200px;">
+          <h2>茉    莉</h2>
+          <p>茉莉，白色手拿包，婚礼上的你，会成为你不可或缺的娇羞小物。</p>
+          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+        </div><!-- /.col-lg-4 -->
+        <div class="col-lg-4">
+          <img class="img-circle" src="detail3.jpg" alt="Generic placeholder image" style="width: 200px; height: 200px;">
+          <h2>简</h2>
+          <p>简，并不简单，能够完全容纳你所有的情绪，也许大凡为简。</p>
+          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+        </div><!-- /.col-lg-4 -->
+      </div><!-- /.row -->
+
+
+      <!-- START THE FEATURETTES -->
+
+      <hr class="featurette-divider">
+
+      <div class="row featurette">
+        <div class="col-md-7">
+          <h2 class="featurette-heading">First featurette heading. <span class="text-muted">It'll blow your mind.</span></h2>
+          <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+        </div>
+        <div class="col-md-5">
+          <img class="featurette-image img-responsive" src="detail4.jpg" alt="黑色口金包">
+        </div>
+      </div>
+
+      <hr class="featurette-divider">
+
+      <div class="row featurette">
+        <div class="col-md-5">
+          <img class="featurette-image img-responsive" src="detail5.jpg" alt="白色口金包">
+        </div>
+        <div class="col-md-7">
+          <h2 class="featurette-heading">Oh yeah, it's that good. <span class="text-muted">See for yourself.</span></h2>
+          <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+        </div>
+      </div>
+
+      <hr class="featurette-divider">
+<!--
+      <div class="row featurette">
+        <div class="col-md-7">
+          <h2 class="featurette-heading">And lastly, this one. <span class="text-muted">Checkmate.</span></h2>
+          <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+        </div>
+        <div class="col-md-5">
+          <img class="featurette-image img-responsive" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
+        </div>
+      </div>
+
+      <hr class="featurette-divider">
+
+-->
+      <!-- /END THE FEATURETTES -->
+
+      <!-- FOOTER -->
+      <footer>
+        <p class="pull-right"><a href="#">Back to top</a></p>
+        <p>&copy; 2014 Company, Inc. &middot; <a href="#">Ibeautyou</a></a></p>
+      </footer>
+
+    </div><!-- /.container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>-->
+<script src="http://cdn.bootcss.com/jquery/1.11.1/jquery.min.js"></script>
+    <!--<script src="../../dist/js/bootstrap.min.js"></script>-->
+<script src="http://cdn.bootcss.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <!--<script src="ie10-viewport-bug-workaround.js"></script>-->
+    <!--<script src="ie-emulation-modes-warning.js"></script>-->
+    <script src="unslide.min.js"></script>
+<script>
+
+			$('.banner').unslider({
+				//arrows: true,
+				fluid: true,
+				//dots: true
+			});
+
+		</script>
+  </body>
+</html>
+
